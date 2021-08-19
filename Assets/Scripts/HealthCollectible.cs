c9c1713f7fa4ef458d0fdcf009318eadb0907e57
dinhhuy997 <dinhquanghuy997@gmail.com>
2021-08-19 23:55:11 +0700

    Add health for ruby

---
+++ Assets/Scripts/HealthCollectible.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthCollectible : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        RubyController controller = collision.GetComponent<RubyController>();
+        if (controller != null && controller.health < controller.maxHealth)
+        {
+            controller.ChangeHealth(1);
+            Destroy(gameObject);
+        }
+    }
+}
