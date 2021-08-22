bd05433ba18124aec25a0c3fb28b77bfca8fa6ec
dinhhuy997 <dinhquanghuy997@gmail.com>
2021-08-23 00:07:41 +0700

    Add animation for Ruby

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -9,6 +9,9 @@ public class RubyController : MonoBehaviour
     public int maxHealth = 5;
     public float timeInvincible = 2.0f;
 
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
     public int health
     {
         get { return currentHealth; }
@@ -25,6 +28,7 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
     }
@@ -44,15 +48,27 @@ public class RubyController : MonoBehaviour
     }
 
     void FixedUpdate() {
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + speed * horizontal * Time.deltaTime;
-        position.y = position.y + speed * vertical * Time.deltaTime;
+        position = position + move * speed * Time.deltaTime;
         rigidbody2d.MovePosition(position);
     }
 
     public void ChangeHealth(int amount)
     {
         if (amount < 0) {
+            animator.SetTrigger("Hit");
             if (isInvincible) {
                 return;
             }
